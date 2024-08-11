2be58838e2b179771f86598b18532c46d3f73ac4
21MoududAhammed <21moudud@gmail.com>
2024-08-11 10:53:51 +0600

    Update the searching process

--- src/components/header/Search.jsx
+++ src/components/header/Search.jsx
@@ -3,29 +3,20 @@ import { useContext, useState } from "react";
 import searchImg from "../../assets/search.svg";
 import { LocationContext } from "../../context";
 import { getLocationByName } from "../../data/location-data";
-// import { useDebounce } from "../../hooks";
+
 
 export default function Search() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const { setLocation } = useContext(LocationContext); // useContext provides location and setLocation
 
-  // useDebounce may use later 
-  // const doSearch = useDebounce((searchTerm) => {
-  //   const fetchLocation = getLocationByName(searchTerm);
-  //   setLocation({ ...fetchLocation });
-  // }, 500);
-
-  // function handleChange(searchTerm) {
-  //   doSearch(searchTerm);
-  // }
-
-  const handleSearch = async (e) =>{
+  const handleSearch = (e) => {
     e.preventDefault();
-    const fetchLocation = await getLocationByName(searchTerm.charAt(0).toUpperCase() + searchTerm.slice(1))
-    console.log(fetchLocation);
-    setLocation({...fetchLocation})
-    setSearchTerm('')
-  }
+    const fetchLocation = getLocationByName(
+      searchTerm.charAt(0).toUpperCase() + searchTerm.slice(1)
+    );
+    setLocation({ ...fetchLocation });
+    setSearchTerm("");
+  };
   return (
     <form action="#">
       <div className="flex items-center space-x-2 py-2 px-3 group focus-within:bg-black/30 transition-all border-b border-white/50 focus-within:border-b-0 focus-within:rounded-md">
@@ -36,9 +27,7 @@ export default function Search() {
           required
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button type="submit" 
-        onClick={handleSearch}
-        >
+        <button type="submit" onClick={handleSearch}>
           <img src={searchImg} />
         </button>
       </div>
